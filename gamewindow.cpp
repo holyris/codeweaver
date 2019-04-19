9badd91caf85de334cc18a9a6ba2aea44aaa30e3
holyris <alexandre.thebault77@gmail.com>
2019-04-19 19:24:21 +0200

    ajout de l'initialisation de la direction du personnage, ainsi que l'initialisation des labels qui affichent les fonctions

--- gamewindow.cpp
+++ gamewindow.cpp
@@ -4,8 +4,8 @@ GameWindow::GameWindow() : QWidget()
 {
 
     std::vector<std::vector<Case*>> cases;
-    unsigned int lim_x = 10, lim_y = 10;
-    Personnage *personnage = new Personnage(9,9,0, lim_x, lim_y);
+    unsigned int lim_x = 10, lim_y = 10, personnage_x=5, personnage_y=5, personnage_direction=0;
+    Personnage *personnage = new Personnage(personnage_x, personnage_y, personnage_direction,lim_x, lim_y);
     Detection *detection = new Detection();
 
     this->setWindowState(Qt::WindowFullScreen); //  on met en fullscreen
@@ -21,31 +21,41 @@ GameWindow::GameWindow() : QWidget()
     grid_main->addWidget(label, 0,1,2,2,0);
 
     //  rempli cases avec des objets Case, et les ajoute au layout de droite
-    for(unsigned int i = 0; i < 10; i++){
+    for(unsigned int i = 0; i < lim_y; i++){
         std::vector<Case*> vector;
-        for(unsigned int j = 0; j < 10; j++){
+        for(unsigned int j = 0; j < lim_x; j++){
 
-            if(i == personnage->getX() && j == personnage->getY()){
-                vector.push_back(new Case(true));
+            if(i == personnage->getY() && j == personnage->getX()){
+                vector.push_back(new Case(true, false));
 
                 grid_right->addWidget(vector.at(j), i, j);
             }
 
             else {
-                vector.push_back(new Case(false));
+                vector.push_back(new Case(false, false));
 
                 grid_right->addWidget(vector.at(j), i, j);
             }
+            vector.at(j)->setDirection(personnage_direction);
         }
         cases.push_back(vector);
     }
 
-    this->controller = new Controller(personnage,cases,detection);
 
-
-    for(unsigned int i = 0; i<10 ; i++){
-        grid_left->addWidget(new QLabel("yo"));
+    std::vector<std::vector<QLabel*>> labels;
+    //il est peu probable que le nb de cartes depasse 20
+    for(unsigned int i = 0; i<20 ; i++){
+        std::vector<QLabel*> vector;
+        for(unsigned int j = 0; j<2 ; j++){
+            vector.push_back(new QLabel(""));
+            grid_left->addWidget(vector.at(j),i,j);
+        }
+        labels.push_back(vector);
     }
+
+
+    this->controller = new Controller(personnage,cases,detection);
+    this->controller->setLabels(labels);
 
 
 
@@ -66,6 +76,6 @@ GameWindow::~GameWindow()
 //  s'execute quand on appuie sur le clavier
 void GameWindow::keyPressEvent(QKeyEvent *event)
 {
-
+    controller->controlCartes();
 //    this->controller->keyPressEvent(event);
 }
