1dd0942a4f4b6c04fd93f00eb1a59d6f25ca7e41
holyris <alexandre.thebault77@gmail.com>
2019-05-09 19:12:00 +0200

    un case ne gère plus si elle possede ou non un cristal ou le personnage. Elle n'a que 2 etats, etre platforme ou non.

--- case.cpp
+++ case.cpp
@@ -2,35 +2,18 @@
 
 Case::Case()
 {
-    srand (time(NULL));
-    this->personnage = false;
-    this->cristal = false;
-    this->personnage_direction = 0;
     this->plateforme = false;
-    this->start_personnage = 0;
-    this->start_cristal = false;
-    this->start_personnage_direction = 0;
-    this->currentFrame = 0;
-    timer = new QTimer();
-    QObject::connect(timer, SIGNAL(timeout()), this, SLOT(animation()));
     randomRotation = 0;
-
-
 }
 
 Case::~Case()
 {
-    delete timer;
 }
-void Case::set(bool personnage, unsigned int personnage_direction, bool cristal, bool plateforme)
+void Case::set(bool plateforme)
 {
-    this->personnage = personnage;
-    this->cristal = cristal;
-    this->personnage_direction = personnage_direction;
+
     this->plateforme = plateforme;
-    start_personnage = personnage;
-    start_cristal = cristal;
-    start_personnage_direction = personnage_direction;
+
     randomRotation = rand() % 4;
     randomMirrorX = rand() % 2;
     randomMirrorY = rand() % 2;
@@ -38,44 +21,6 @@ void Case::set(bool personnage, unsigned int personnage_direction, bool cristal,
     if(randomMirrorX==0) randomMirrorX = -1;
     if(randomMirrorY==0) randomMirrorY = -1;
     this->display();
-}
-void Case::setPersonnage(bool personnage)
-{
-    this->personnage = personnage;
-    this->display();
-}
-
-void Case::setCristal(bool cristal)
-{
-    this->cristal = cristal;
-    this->display();
-}
-
-
-void Case::setDirection(unsigned int personnage_direction)
-{
-    this->personnage_direction = personnage_direction;
-    this->display();
-}
-
-//  pour revenir a l'etat de debut de partie
-void Case::reset()
-{
-    personnage = start_personnage;
-    cristal = start_cristal;
-    personnage_direction = start_personnage_direction;
-    this->display();
-}
-
-void Case::deleteCristal()
-{
-    if(cristal){
-        cristal = false;
-        timer->start(150);
-        qApp->processEvents();
-    }
-
-
 }
 
 //  affiche en fonction de l'etat de la case
@@ -85,9 +30,7 @@ void Case::display()
     this->setSizePolicy( QSizePolicy::Ignored, QSizePolicy::Ignored );
     QPixmap pix;
 
-    if(isCristal()){
-        pix.load("images/plateforme.png");
-    } else if(isPlateforme()){
+    if(isPlateforme()){
         pix.load("images/plateforme.png");
     }
 
@@ -97,49 +40,9 @@ void Case::display()
     this->repaint();
 }
 
-
-
-bool Case::isPersonnage() const
-{
-    return personnage;
-}
-
-bool Case::isCristal() const
-{
-    return cristal;
-}
-
 bool Case::isPlateforme() const
 {
     return plateforme;
 }
 
-void Case::animation()
-{
-//    currentFrame++;
-//    if(currentFrame==1){
-//        this->setScaledContents( true );
-//        this->setSizePolicy( QSizePolicy::Ignored, QSizePolicy::Ignored );
-//        this->setPixmap(QPixmap("images/plateforme+explosion1.png"));
-//        this->repaint();
-//    }
-//    else if(currentFrame==2){
-//        this->setScaledContents( true );
-//        this->setSizePolicy( QSizePolicy::Ignored, QSizePolicy::Ignored );
-//        this->setPixmap(QPixmap("images/plateforme+explosion2.png"));
-//        this->repaint();
-//    }
-//    else if(currentFrame==3){
-//        this->setScaledContents( true );
-//        this->setSizePolicy( QSizePolicy::Ignored, QSizePolicy::Ignored );
-//        this->setPixmap(QPixmap("images/plateforme+explosion3.png"));
-//        this->repaint();
-//    }
-//    else {
-//        timer->stop();
-//        this->display();
-//    }
 
-
-
-}
--- case.h
+++ case.h
@@ -5,43 +5,21 @@
 #include <QWidget>
 #include <QLabel>
 #include <QPainter>
-#include <QTimer>
 class Case : public QLabel
 {
-
-    Q_OBJECT
-
 private:
 
-    QTimer *timer;
-    int currentFrame;
     int randomRotation, randomMirrorX, randomMirrorY;
-    bool cristal;
-    bool personnage;
-    unsigned int personnage_direction;
-    bool plateforme;       // une case reste toujours en plateforme donc il n'ya pas de start_plateforme
-    bool start_personnage;
-    bool start_cristal;
-    unsigned int start_personnage_direction;
+    bool plateforme;
     void display();
 
 public:
     Case();
     ~Case();
-    void set(bool personnage, unsigned int personnage_direction, bool cristal, bool plateforme);
-    void setPersonnage(bool personnage);
-    void setCristal(bool cristal);
-    void setDirection(unsigned int personnage_direction);
-    void reset();
+    void set(bool plateforme);
     void deleteCristal();
 
-    bool isPersonnage() const;
-    bool isCristal() const;
     bool isPlateforme() const;
-
-private slots:
-    void animation();
-
 };
 
 #endif // CASE_H
